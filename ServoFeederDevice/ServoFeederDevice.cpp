57aca73008dc1f5d5f1a2a197f38b1440c0c244b
DTellurian <d.mihalych@gmail.com>
2015-09-09 22:32:57 +0300

    CreatedProject

---
+++ ServoFeederDevice/ServoFeederDevice.cpp
@@ -0,0 +1,17 @@
+/*
+ * ServoFeederDevice.cpp
+ *
+ * Created: 09.09.2015 15:32:22
+ *  Author: Mihalych
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
